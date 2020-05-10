9b13c3eb09a8235628b91bbf00a2c4a49670fea3
rajwindersidhu92 <kaurrajwinder203@gmail.com>
2020-05-10 17:20:25 -0400

    Adding collection view label

--- Search Word/ViewController.swift
+++ Search Word/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     
 
     @IBOutlet weak var collection_view: UICollectionView!
-    
+    let gird_size = 10
+    var count = 0
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         collection_view.delegate = self
         collection_view.dataSource = self
@@ -25,13 +27,14 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return gird_size * gird_size
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        print(1)
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection_cell", for: indexPath) as! CollectionViewCellController
         cell.celllabel?.text = "a"
+        count = count + 1
+        print(count)
         return cell
     }
         
