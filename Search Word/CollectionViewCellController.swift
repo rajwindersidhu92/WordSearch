bba47a677a326952db7ab9e482226e610547800d
rajwindersidhu92 <kaurrajwinder203@gmail.com>
2020-05-10 00:15:51 -0400

    collection view added

---
+++ Search Word/CollectionViewCellController.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionViewCellController.swift
+//  Search Word
+//
+//  Created by Rajwinder Kaur on 2020-05-09.
+//  Copyright © 2020 Rajwinder Kaur. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCellController:UICollectionViewCell{
+
+@IBOutlet weak var celllabel : UILabel!
+
+    /*
+ // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Search Word/ViewController.swift
+++ Search Word/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
 
+    @IBOutlet weak var collection_view: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collection_view.delegate = self
+        collection_view.dataSource = self
+        // Do any additional setup after loading the view
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        print(indexPath);
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        print(1)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection_cell", for: indexPath) as! CollectionViewCellController
+        cell.celllabel?.text = "a"
+        return cell
+    }
+        
 
 }
 
